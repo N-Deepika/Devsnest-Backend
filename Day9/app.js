703332a9d31d58e54cdd1d81667aaf66369914ed
N-Deepika <deepika282002@gmail.com>
2021-09-17 23:23:08 +0530

    THAs

--- Day9/app.js
+++ Day9/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const path = require('path')
+const path = require('path');
 const app = express();
+app.set("view engine", "pug")
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./public", "btsArmy.jpg"));
+    // res.sendFile(path.resolve(__dirname, "./public", "btsArmy.jpg"));
+    res.render("index");
 })
 app.listen(3000);
