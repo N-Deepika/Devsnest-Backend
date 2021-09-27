67e5cda4de2b652db0cf10ca9873fbbb4283ff0a
N-Deepika <deepika282002@gmail.com>
2021-09-27 22:34:06 +0530

    connected the db

--- Day11-12/controllers/register.js
+++ Day11-12/controllers/register.js
@@ -1,33 +1,32 @@
-const User = require('../models/user');
-// check if email already exists
-// password Hash
-// email lowercase
-// save
+const User = require('../models/mongo');
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
+/**
+ * Check if email already exists
+ * password hashing - security
+ * email lowercase
+ * save
+ */
+
+const saltRounds = 10;
 const register = async(req, res) => {
     const { email, password } = req.body;
+
     try {
         const alreadyExists = await User.findOne({ where: { email } });
         if (alreadyExists) {
             res.status(401).send("Email already exists");
-        } else {
-            const salt = bcrypt.genSaltSync(saltRounds);
-            const hash = bcrypt.hashSync(password, salt);
-
-            const newUser = new User({
-                email: email.toLowerCase(),
-                password: hash,
-            });
-            const savedUser = await newUser.save();
-            res.status(201).send("User Registered");
         }
-    } catch (err) {
-        console.log("Error", err);
-        res.status(500).send("Something Went Wrong");
-    }
-};
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
 
+        const newUser = new User({ email: email.toLowerCase(), password: password, fullName: "light" })
+        const savedUser = await newUser.save();
+        res.status(201).send(savedUser);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Something went wrong");
+    }
+}
 
 module.exports = register;
