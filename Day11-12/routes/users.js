93d9869753535540885baa5944fdc43444e26bb6
N-Deepika <deepika282002@gmail.com>
2021-09-27 23:26:36 +0530

    connected the db

--- Day11-12/routes/users.js
+++ Day11-12/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '9876@12345',
+    port: 3000
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    pool.query('SELECT * FROM "USERS"', (err, result) => {
+            if (err) {
+                throw err;
+            }
+            res.status(200).json(results);
+        })
+        // res.send('respond with a resource');
 });
 
 module.exports = router;
+
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
